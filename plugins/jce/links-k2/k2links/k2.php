5d3023f6107998658011275c9ba319cf1406bea8
Ryan Demmer <ryandemmer@gmail.com>
2017-09-05 10:31:53 +0100

    Add support for anchors in item content

--- plugins/jce/links-k2/k2links/k2.php
+++ plugins/jce/links-k2/k2links/k2.php
@@ -15,178 +15,186 @@ defined('_WF_EXT') or die('ERROR_403');
 class K2linksK2 extends JObject
 {
 
-	var $_option = 'com_k2';
-	var $_task = 'category';
+    public $_option = 'com_k2';
+    public $_task = 'category';
 
-	/**
-	 * Constructor activating the default information of the class
-	 *
-	 * @access	protected
-	 */
-	function __construct($options = array())
-	{
-	}
+    /**
+     * Constructor activating the default information of the class
+     *
+     * @access    protected
+     */
+    public function __construct($options = array())
+    {
+    }
 
-	/**
-	 * Returns a reference to a editor object
-	 *
-	 * This method must be invoked as:
-	 * 		<pre>  $browser =JContentEditor::getInstance();</pre>
-	 *
-	 * @access	public
-	 * @return	JCE  The editor object.
-	 * @since	1.5
-	 */
-	function getInstance()
-	{
-		static $instance;
+    /**
+     * Returns a reference to a editor object
+     *
+     * This method must be invoked as:
+     *         <pre>  $browser =JContentEditor::getInstance();</pre>
+     *
+     * @access    public
+     * @return    JCE  The editor object.
+     * @since    1.5
+     */
+    public function getInstance()
+    {
+        static $instance;
 
-		if (!is_object($instance))
-		{
-			$instance = new K2linksK2();
-		}
-		return $instance;
-	}
+        if (!is_object($instance)) {
+            $instance = new K2linksK2();
+        }
+        return $instance;
+    }
 
-	public function getOption()
-	{
-		return $this->_option;
-	}
+    public function getOption()
+    {
+        return $this->_option;
+    }
 
-	public function getTask()
-	{
-		return $this->_task;
-	}
+    public function getTask()
+    {
+        return $this->_task;
+    }
 
-	public function getList()
-	{
-		$advlink = WFEditorPlugin::getInstance();
-		$list = '';
-		if ($advlink->checkAccess('k2links.k2', '1'))
-		{
-			$list = '<li id="index.php?option=com_k2&task=category"><div class="uk-tree-row"><a href="#"><span class="uk-tree-icon folder content nolink"></span><span class="uk-tree-text">'.JText::_('K2 Categories &amp; Items').'</span></a></div></li>';
-		}
-		return $list;
-	}
+    public function getList()
+    {
+        $advlink = WFEditorPlugin::getInstance();
+        $list = '';
+        if ($advlink->checkAccess('k2links.k2', '1')) {
+            $list = '<li id="index.php?option=com_k2&task=category"><div class="uk-tree-row"><a href="#"><span class="uk-tree-icon folder content nolink"></span><span class="uk-tree-text">' . JText::_('K2 Categories &amp; Items') . '</span></a></div></li>';
+        }
+        return $list;
+    }
 
-	public static function _getK2Categories($parent_id = 0)
-	{
-		// K2 2.x
-		if (defined('K2_JVERSION'))
-		{
-			$db = JFactory::getDBO();
-			$query = 'SELECT id, name, alias FROM #__k2_categories WHERE published = 1';
-			$user = JFactory::getUser();
-			if (version_compare(JVERSION, '1.6.0', 'ge'))
-			{
-				$query .= ' AND `access` IN ('.implode(',', $user->getAuthorisedViewLevels()).')';
-			}
-			else
-			{
-				$query .= "\nAND `access` <=".(int)$user->get('aid');
-			}
-			$query .= ' AND parent = '.$db->Quote($parent_id).' ORDER BY ordering ASC';
-			$db->setQuery($query);
-			$rows = $db->loadObjectList();
-		}
-		// K2 3.x
-		else
-		{
-			if ($parent_id == 0)
-			{
-				$parent_id = 1;
-			}
-			$model = K2Model::getInstance('Categories');
-			$model->setState('site', true);
-			$model->setState('parent', $parent_id);
-			$model->setState('sorting', 'ordering');
-			$rows = $model->getRows();
-		}
+    public static function _getK2Categories($parent_id = 0)
+    {
+        // K2 2.x
+        if (defined('K2_JVERSION')) {
+            $db = JFactory::getDBO();
+            $query = 'SELECT id, name, alias FROM #__k2_categories WHERE published = 1';
+            $user = JFactory::getUser();
+            if (version_compare(JVERSION, '1.6.0', 'ge')) {
+                $query .= ' AND `access` IN (' . implode(',', $user->getAuthorisedViewLevels()) . ')';
+            } else {
+                $query .= "\nAND `access` <=" . (int) $user->get('aid');
+            }
+            $query .= ' AND parent = ' . $db->Quote($parent_id) . ' ORDER BY ordering ASC';
+            $db->setQuery($query);
+            $rows = $db->loadObjectList();
+        }
+        // K2 3.x
+        else {
+            if ($parent_id == 0) {
+                $parent_id = 1;
+            }
+            $model = K2Model::getInstance('Categories');
+            $model->setState('site', true);
+            $model->setState('parent', $parent_id);
+            $model->setState('sorting', 'ordering');
+            $rows = $model->getRows();
+        }
 
-		return $rows;
-	}
+        return $rows;
+    }
 
-	public static function _getK2Items($category_id = 0)
-	{
-		// K2 2.x
-		if (defined('K2_JVERSION'))
-		{
-			$db = JFactory::getDBO();
+    public static function _getK2Items($category_id = 0)
+    {
+        // K2 2.x
+        if (defined('K2_JVERSION')) {
+            $db = JFactory::getDBO();
 
-			$query = 'SELECT id, title, alias, catid FROM #__k2_items WHERE published = 1';
+            $query = 'SELECT id, title, alias, CONCAT(' . $db->quoteName('introtext') . ',' . $db->quoteName('fulltext') . ') AS content FROM #__k2_items WHERE published = 1';
 
-			$user = JFactory::getUser();
-			if (version_compare(JVERSION, '1.6.0', 'ge'))
-			{
-				$query .= ' AND `access` IN ('.implode(',', $user->getAuthorisedViewLevels()).')';
-			}
-			else
-			{
-				$query .= "\nAND `access` <=".(int)$user->get('aid');
-			}
+            $user = JFactory::getUser();
+            if (version_compare(JVERSION, '1.6.0', 'ge')) {
+                $query .= ' AND `access` IN (' . implode(',', $user->getAuthorisedViewLevels()) . ')';
+            } else {
+                $query .= "\nAND `access` <=" . (int) $user->get('aid');
+            }
 
-			$query .= ' AND catid = '.$db->Quote($category_id).' ORDER BY ordering ASC';
+            $query .= ' AND catid = ' . $db->Quote($category_id) . ' ORDER BY ordering ASC';
 
-			$db->setQuery($query);
-			$rows = $db->loadObjectList();
-		}
-		// K2 3.x
-		else
-		{
-			$model = K2Model::getInstance('Items');
-			$model->setState('site', true);
-			$model->setState('category', (int)$category_id);
-			$model->setState('recursive', false);
-			$model->setState('sorting', 'ordering');
-			$rows = $model->getRows();
-		}
-		return $rows;
-	}
+            $db->setQuery($query);
+            $rows = $db->loadObjectList();
+        }
+        // K2 3.x
+        else {
+            $model = K2Model::getInstance('Items');
+            $model->setState('site', true);
+            $model->setState('category', (int) $category_id);
+            $model->setState('recursive', false);
+            $model->setState('sorting', 'ordering');
+            $rows = $model->getRows();
+        }
+        return $rows;
+    }
 
-	function getLinks($args)
-	{
-		$mainframe = JFactory::getApplication();
+    public function getLinks($args)
+    {
+        $mainframe = JFactory::getApplication();
 
-		$advlink = WFEditorPlugin::getInstance();
-		if (defined('K2_JVERSION'))
-		{
-			require_once (JPATH_SITE.DS.'components'.DS.'com_k2'.DS.'helpers'.DS.'route.php');
-		}
+        $advlink = WFEditorPlugin::getInstance();
+        if (defined('K2_JVERSION')) {
+            require_once JPATH_SITE . DS . 'components' . DS . 'com_k2' . DS . 'helpers' . DS . 'route.php';
+        }
 
-		$items = array();
-		$view = isset($args->view) ? $args->view : '';
+        $items = array();
+        $view = isset($args->view) ? $args->view : '';
 
-		switch ($view)
-		{
+        switch ($view) {
 
-			default :
-				$categories = self::_getK2Categories();
-				foreach ($categories as $category)
-				{
-					$category->href = K2HelperRoute::getCategoryRoute($category->id);
-					$items[] = array('id' => $category->href, 'name' => $category->name, 'class' => 'folder content');
-				}
-				break;
+            default:
+                $categories = self::_getK2Categories();
+                foreach ($categories as $category) {
+                    $category->href = K2HelperRoute::getCategoryRoute($category->id);
+                    $items[] = array('id' => $category->href, 'name' => $category->name, 'class' => 'folder content');
+                }
+                break;
 
-			case 'itemlist' :
-				$categories = self::_getK2Categories($args->id);
-				$itemlist = self::_getK2Items($args->id);
-				foreach ($categories as $category)
-				{
-					$category->href = K2HelperRoute::getCategoryRoute($category->id);
-					$items[] = array('id' => $category->href, 'name' => $category->name, 'class' => 'folder content');
-				}
-				foreach ($itemlist as $item)
-				{
-					$item->href = K2HelperRoute::getItemRoute($item->id.':'.$item->alias, $item->catid);
+            case 'itemlist':
+                $categories = self::_getK2Categories($args->id);
+                $itemlist = self::_getK2Items($args->id);
+                foreach ($categories as $category) {
+                    $category->href = K2HelperRoute::getCategoryRoute($category->id);
+                    $items[] = array('id' => $category->href, 'name' => $category->name, 'class' => 'folder content');
+                }
+                foreach ($itemlist as $item) {
+                    $item->href = K2HelperRoute::getItemRoute($item->id . ':' . $item->alias, $item->catid);
 					$items[] = array('id' => $item->href, 'name' => $item->title, 'class' => 'file');
-				}
-				break;
 
-			case 'item' :
-				break;
-		}
-		return $items;
-	}
+                    $anchors = self::getAnchors($item->content);
 
+                    foreach ($anchors as $anchor) {
+                        $items[] = array(
+                            'id' => $item->href . '#' . $anchor,
+                            'name' => '#' . $anchor,
+                            'class' => 'file anchor',
+                        );
+                    }
+                }
+                break;
+
+            case 'item':
+                break;
+        }
+        return $items;
+    }
+
+    private static function getAnchors($content)
+    {
+        preg_match_all('#<a([^>]+)(name|id)="([a-z]+[\w\-\:\.]*)"([^>]*)>#i', $content, $matches, PREG_SET_ORDER);
+
+        $anchors = array();
+
+        if (!empty($matches)) {
+            foreach ($matches as $match) {
+                if (strpos($match[0], 'href') === false) {
+                    $anchors[] = $match[3];
+                }
+            }
+        }
+
+        return $anchors;
+    }
 }
